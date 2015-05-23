816cf7d6ecc3f9b8928071e28fae696d482212ab
raviteja642581 <chendiraviteja@gmail.com>
2015-05-23 14:58:57 -0400

    Soney 1

--- app/src/main/java/com/charlesli/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/charlesli/rockpaperscissors/MainActivity.java
@@ -5,14 +5,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.view.View.OnKeyListener;
 
-public class MainActivity extends ActionBarActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+            }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
