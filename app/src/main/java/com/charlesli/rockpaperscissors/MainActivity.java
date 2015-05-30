840eb151a65ec902f3407c41529e13ea5b662cd3
--add <jordan_li80@hotmail.com>
2015-05-30 11:52:02 -0400

    Shared Preferences Intro

--- app/src/main/java/com/charlesli/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/charlesli/rockpaperscissors/MainActivity.java
@@ -3,6 +3,7 @@ package com.charlesli.rockpaperscissors;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
@@ -11,17 +12,25 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity{
 
     private EditText name;
     private Button gobutton;
+    private SharedPreferences sharedInfo;
+    private String savedName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedInfo = getSharedPreferences("SHARED_RPS", MODE_PRIVATE);
+        //savedName = sharedInfo.getString("NAME", "Charles");
+
+
         name = (EditText) findViewById(R.id.editTextResult);
         gobutton = (Button) findViewById(R.id.buttonStop);
 
@@ -33,9 +42,19 @@ public class MainActivity extends ActionBarActivity{
         gobutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent screen = new Intent(MainActivity.this, OptionsActivity.class);
-                screen.putExtra("NAME", name.getText().toString());
-                startActivity(screen);
+                if (!name.getText().toString().equals("")) {
+                    SharedPreferences.Editor editor = sharedInfo.edit();
+                    editor.putString("NAME", name.getText().toString());
+                    editor.commit();
+
+                    Intent screen = new Intent(MainActivity.this, OptionsActivity.class);
+                    //screen.putExtra("NAME", name.getText().toString());
+                    startActivity(screen);
+                }
+                else {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Please input a name", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
     }
