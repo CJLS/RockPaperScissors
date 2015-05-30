ab4cacc86750dc7d7b4ed1c47c46eb03e8f81cc6
--add <jordan_li80@hotmail.com>
2015-05-30 11:21:56 -0400

    Restrict Back Button (Alert Dialog)

--- app/src/main/java/com/charlesli/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/charlesli/rockpaperscissors/MainActivity.java
@@ -1,8 +1,11 @@
 package com.charlesli.rockpaperscissors;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends ActionBarActivity{
 
     private EditText name;
     private Button gobutton;
@@ -21,6 +24,10 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
         name = (EditText) findViewById(R.id.editTextResult);
         gobutton = (Button) findViewById(R.id.buttonStop);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Home Page");
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
 
         gobutton.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +47,6 @@ public class MainActivity extends Activity{
     }
 
 
-    //GEEEE
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -52,7 +58,37 @@ public class MainActivity extends Activity{
         if (id == R.id.action_settings) {
             return true;
         }
+        else if (id == android.R.id.home) {
+            onBackPressed();
+            System.out.println("Back button pressed");
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Do you want to quit")
+                .setMessage("Really??")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+        System.out.println("Back button pressed");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Resumed");
     }
 }
